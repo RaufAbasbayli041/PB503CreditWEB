50d22b51d56325e4c64b950c53a10aeeaaab833b
RaufAbasbayli041 <abasbeyli97@gmail.com>
2025-05-08 23:18:22 +0400

    PB503 MVC CreditWEb1

---
+++ CredidSystem/Entity/Branch.cs
@@ -0,0 +1,13 @@
+﻿namespace CredidSystem.Entity
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int MerchantId { get; set; }
+        public Merchant Merchant { get; set; } // Navigation property
+    }
+}
---
+++ CredidSystem/Entity/Merchant.cs
@@ -0,0 +1,10 @@
+﻿namespace CredidSystem.Entity
+{
+    public class Merchant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Branch> Branches { get; set; } // Navigation property
+    }
+}
